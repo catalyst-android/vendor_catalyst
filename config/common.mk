ProjectSakura/vendor_sakura

Vendor: only build updater when its official
Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
Change-Id: Ie93c035fc879bf55d9484ecce1fb3a2c27910105
Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

config/common.mk
@@ -113,8 +113,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
-    Updater
+    LineageSetupWizard
 
 # Themes
 PRODUCT_PACKAGES += \
@@ -193,11 +192,13 @@ DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
 
 PRODUCT_VERSION_MAJOR = 3
 PRODUCT_VERSION_MINOR = R
-SAKURA_BUILD := ALPHA
+SAKURA_BUILD := UNOFFICIAL-ALPHA
 
 #Official and unofficial for the sake of the world
 ifeq ($(SAKURA_OFFICIAL), true)
-    SAKURA_BUILD := OFFICIAL
+    SAKURA_BUILD := OFFICIAL-ALPHA
+    PRODUCT_PACKAGES += \
+    Updater
 endif
 
 #Gapps

catalyst-android/vendor_catalyst

config/common.mk
@@ -114,8 +114,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
-    Updater
+    LineageSetupWizard
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
@@ -210,11 +209,13 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 PRODUCT_VERSION_MAJOR = 0
 PRODUCT_VERSION_MINOR = 1
-CATALYST_BUILD := ALPHA
+CATALYST_BUILD := UNOFFICIAL-ALPHA
 
 #Official and unofficial for the sake of the world
 ifeq ($(CATALYST_OFFICIAL), true)
     CATALYST_BUILD := OFFICIAL
+    PRODUCT_PACKAGES += \
+    Updater
 endif
 
 #Gapps