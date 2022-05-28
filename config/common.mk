ProjectSakura/vendor_sakura

Config: add flags for GAPPS
Also don't build some apps like calendar and email coz truth be told nobody likes these.

Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

config/common.mk
@@ -201,6 +201,7 @@ DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/common
 PRODUCT_VERSION_MAJOR = 3
 PRODUCT_VERSION_MINOR = R
 SAKURA_BUILD := UNOFFICIAL-BETA
+SAKURA_BUILD_ZIP_TYPE := VANILLA
 
 #Official and unofficial for the sake of the world
 ifeq ($(SAKURA_OFFICIAL), true)
@@ -210,13 +211,13 @@ ifeq ($(SAKURA_OFFICIAL), true)
 endif
 
 #Gapps
-#ifeq ($(SAKURA_GAPPS), true)
-#￼   $(call inherit-product, vendor/gapps/config.mk)
-#￼   SAKURA_BUILD_ZIP_TYPE := GAPPS
-#endif
+ifeq ($(SAKURA_GAPPS), true)
+    $(call inherit-product, vendor/gapps/gapps.mk)
+    SAKURA_BUILD_ZIP_TYPE := GAPPS
+endif
 
 # Increase sakura Version with each major release.
-LINEAGE_VERSION := ProjectSakura-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d-%H%M)-$(LINEAGE_BUILD)-$(SAKURA_BUILD)
+LINEAGE_VERSION := ProjectSakura-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(SAKURA_BUILD_ZIP_TYPE)-$(shell date +%Y%m%d-%H%M)-$(LINEAGE_BUILD)-$(SAKURA_BUILD)
 LINEAGE_DISPLAY_VERSION := ProjectSakura-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-$(SAKURA_BUILD)
 SAKURA_VERSION := $(LINEAGE_VERSION)
 

catalyst-android/vendor_catalyst

Change-Id: I136bbf64e736b12688524f308a9a5c8efa4ba159

config/common.mk
@@ -211,6 +211,7 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 PRODUCT_VERSION_MAJOR = 0
 PRODUCT_VERSION_MINOR = 1
 CATALYST_BUILD := UNOFFICIAL-ALPHA
+CATALYST_BUILD_ZIP_TYPE := VANILLA
 
 #Official and unofficial for the sake of the world
 ifeq ($(CATALYST_OFFICIAL), true)
@@ -221,12 +222,12 @@ endif
 
 #Gapps
 ifeq ($(CATALYST_GAPPS), true)
-    $(call inherit-product, vendor/gapps/config.mk)
+    $(call inherit-product, vendor/gapps/gapps.mk)
     CATALYST_BUILD_ZIP_TYPE := GAPPS
 endif
 
 # Increase catalyst Version with each major release.
-LINEAGE_VERSION := CatalystOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d-%H%M)-$(LINEAGE_BUILD)-$(CATALYST_BUILD)
+LINEAGE_VERSION := CatalystOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(CATALYST_BUILD_ZIP_TYPE)-$(shell date +%Y%m%d-%H%M)-$(LINEAGE_BUILD)-$(CATALYST_BUILD)
 LINEAGE_DISPLAY_VERSION := CatalystOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-$(CATALYST_BUILD)
 CATALYST_VERSION := $(LINEAGE_VERSION)
 