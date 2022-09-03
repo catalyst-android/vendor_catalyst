1f202a305d4854d7b97ba4af4572a12c334aa0d4
LordShenron <shen.priyanshu@gmail.com>
2022-09-03 17:10:38 +0530

    Vendor: tweak versioning
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,50 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CATALYST_MOD_VERSION = v13.0
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+
+CATALYST_MOD_VERSION = 1.0
 CATALYST_BUILD_TYPE := UNOFFICIAL
-CATALYST_BUILD_ZIP_TYPE := VANILLA
+CATALYST_BUILD_ZIP_TYPE := GAPPS
 
 ifeq ($(CATALYST_BETA),true)
     CATALYST_BUILD_TYPE := BETA
 endif
 
-ifeq ($(CATALYST_GAPPS), true)
-    $(call inherit-product, vendor/gapps/common/common-vendor.mk)
-    CATALYST_BUILD_ZIP_TYPE := GAPPS
-endif
-
 CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
 ifeq ($(CATALYST_OFFICIAL), true)
-   LIST = $(shell cat infrastructure/devices/catalyst.devices | awk '$$1 != "#" { print $$2 }')
-    ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
-      IS_OFFICIAL=true
-      CATALYST_BUILD_TYPE := OFFICIAL
+    IS_OFFICIAL=true
+    CATALYST_BUILD_TYPE := OFFICIAL
 
 PRODUCT_PACKAGES += \
     Updater
 
-    endif
-    ifneq ($(IS_OFFICIAL), true)
-       CATALYST_BUILD_TYPE := UNOFFICIAL
-       $(error Device is not official "$(CURRENT_DEVICE)")
-    endif
 endif
 
-ifeq ($(CATALYST_COMMUNITY), true)
-   LIST = $(shell cat infrastructure/devices/catalyst-community.devices | awk '$$1 != "#" { print $$2 }')
-    ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
-      IS_COMMUNITY=true
-      CATALYST_BUILD_TYPE := COMMUNITY
-    endif
-    ifneq ($(IS_COMMUNITY), true)
-       CATALYST_BUILD_TYPE := UNOFFICIAL
-       $(error This isn't a community device "$(CURRENT_DEVICE)")
-    endif
-endif
-
-CATALYST_VERSION := Catalyst-$(CATALYST_MOD_VERSION)-$(CURRENT_DEVICE)-$(CATALYST_BUILD_TYPE)-$(shell date -u +%Y%m%d)-$(CATALYST_BUILD_ZIP_TYPE)
+CATALYST_VERSION := CatalystOS-$(CATALYST_MOD_VERSION)-$(CATALYST_BUILD_TYPE)-$(shell date -u +%Y%m%d)-$(CATALYST_BUILD_ZIP_TYPE)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.catalyst.version=$(CATALYST_VERSION) \
@@ -64,7 +42,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.catalyst.ziptype=$(CATALYST_BUILD_ZIP_TYPE) \
   ro.modversion=$(CATALYST_MOD_VERSION)
 
-CATALYST_DISPLAY_VERSION := Catalyst-$(CATALYST_MOD_VERSION)-$(CATALYST_BUILD_TYPE)
+CATALYST_DISPLAY_VERSION := CatalystOS-$(CATALYST_MOD_VERSION)-$(CATALYST_BUILD_TYPE)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.catalyst.display.version=$(CATALYST_DISPLAY_VERSION)
