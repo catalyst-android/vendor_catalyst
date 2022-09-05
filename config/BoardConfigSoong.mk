bfa003f56ad7bd0c838ab5e8a485ff94837f7444
LordShenron <shen.priyanshu@gmail.com>
2022-09-05 15:35:38 +0000

    Vendor: add some soong variables camera
    - change needs_camera_boottime to needs_camera_boottime_defaults
    - Add camera_in_mediaserver and few more
    
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,7 +33,9 @@ SOONG_CONFIG_catalystGlobalVars += \
     target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
-    uses_egl_display_array
+    uses_egl_display_array \
+    camera_needs_client_info_lib \
+    has_legacy_camera_hal1
 
 SOONG_CONFIG_NAMESPACES += catalystNvidiaVars
 SOONG_CONFIG_catalystNvidiaVars += \
@@ -43,7 +45,7 @@ SOONG_CONFIG_NAMESPACES += catalystQcomVars
 SOONG_CONFIG_catalystQcomVars += \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd \
-    needs_camera_boottime_timestamp
+    needs_camera_boottime_timestamp_defaults
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -55,8 +57,10 @@ endif
 SOONG_CONFIG_catalystNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_catalystQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_catalystQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_catalystQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_catalystQcomVars_needs_camera_boottime_timestamp_defaults := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_catalystGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
+SOONG_CONFIG_catalystGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
+SOONG_CONFIG_catalystGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
@@ -71,6 +75,7 @@ SOONG_CONFIG_catalystGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_L
 SOONG_CONFIG_catalystGlobalVars_target_inputdispatcher_skip_event_key := $(TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY)
 SOONG_CONFIG_catalystGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_catalystGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
+SOONG_CONFIG_catalystGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_catalystQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
 else
